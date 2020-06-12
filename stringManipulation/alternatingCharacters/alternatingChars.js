e76e9cb424c97a5ecac8855cfbf8e25d03a77a06
David <daveshuang@gmail.com>
2020-06-12 14:41:35 -0700

    completed - removed modifying the array just counting consecutive matching letters now

--- stringManipulation/alternatingCharacters/alternatingChars.js
+++ stringManipulation/alternatingCharacters/alternatingChars.js
@@ -31,9 +31,7 @@ function alternatingCharacters(s) {
 
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === arr[i+1]) {
-            arr.shift();
             minDeletions++;
-            i--;
         }
     }
 
