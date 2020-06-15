a0ce1d61fb0c2741fd3372835d87f19afdc55ba2
David <daveshuang@gmail.com>
2020-06-15 16:16:55 -0700

    special string again

---
+++ stringManipulation/specialStringAgain/specialStringAgain.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the substrCount function below.
+function substrCount(n, s) {
+    let count = n;
+
+    let res1 = s.match(/([a-z])\1+/g);
+
+    let triangular = (x) => {
+        return (x * x + x) / 2;
+    };
+
+    if (res1 != null) {
+        res1.forEach((item, index) => {
+            count += triangular(item.length - 1);
+        });
+
+    }
+
+    res1 = s.match(/(([a-z])\2*)(?!\1)(?=[a-z]\1)/g);
+
+    if (res1 != null) {
+        res1.forEach((item, index) => {
+            count += (item.length);
+        });
+
+    }
+
+    return count;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const s = readLine();
+
+    const result = substrCount(n, s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
