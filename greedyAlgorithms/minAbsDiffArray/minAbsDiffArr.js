01e6e80f03ebece6ab7efc1c87d1ffff49462335
David <daveshuang@gmail.com>
2020-06-18 17:06:38 -0700

    minimum absolute difference in a array

---
+++ greedyAlgorithms/minAbsDiffArray/minAbsDiffArr.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the minimumAbsoluteDifference function below.
+function minimumAbsoluteDifference(arr) {
+    arr.sort();
+    let min = Math.abs(arr[arr.length - 1] - arr[0]);
+    for (let i = 0; i < arr.length - 1; i++) {
+        if ( Math.abs(arr[i + 1] - arr[i]) < min) {
+            min = Math.abs(arr[i + 1] - arr[i]);
+        }
+    }
+    return min;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = minimumAbsoluteDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
